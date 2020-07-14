84b57138ce865cc7ce99fc4a492fff91158deb4a
Cyn3ah <62955916+Cyn3ah@users.noreply.github.com>
2020-07-14 10:49:08 -0500

    fixed heal

--- assets/javascript/TravelerClass.js
+++ assets/javascript/TravelerClass.js
@@ -24,4 +24,48 @@ class Traveler {
 
 }
 
-// console.log(Traveler)
+class Doctor extends Traveler {
+    constructor (name, school) {
+    super(name)
+    this.school = school
+    }
+
+    heal (traveler) {
+
+        traveler.isHealthy = true
+
+    }
+
+}
+
+class Hunter extends Traveler {
+    constructor (name) {
+    super(name)
+    this.food = 2
+    }
+
+    hunt () {
+
+        this.food += 5
+    }
+
+    eat () {
+
+        if (this.food > 1) {
+            this.food -= 2
+        } else {
+            this.food -= 1
+            this.isHealthy = false
+        }
+    }
+
+    giveFood (traveler, numOfFoodUnits) {
+
+        if (this.food >= numOfFoodUnits) {
+            this.food -= numOfFoodUnits
+            traveler.food += numOfFoodUnits
+        } 
+
+    }
+
+}
